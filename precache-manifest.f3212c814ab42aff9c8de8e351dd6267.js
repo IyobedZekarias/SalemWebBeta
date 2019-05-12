2c2df911ebad55b928eb8e646819cf6c711b4533
Iyobed Zekarias <IyobedZekarias@users.noreply.github.com>
2019-05-12 16:05:44 -0500

    Updates

---
+++ precache-manifest.f3212c814ab42aff9c8de8e351dd6267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1623b19b09745fe5f317b1cae8a3650c",
+    "url": "/SalemWebBeta/index.html"
+  },
+  {
+    "revision": "1cfb4b555f0d223cd4ee",
+    "url": "/SalemWebBeta/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "894f2192d97a722b05c8",
+    "url": "/SalemWebBeta/static/css/main.0dd60e14.chunk.css"
+  },
+  {
+    "revision": "1cfb4b555f0d223cd4ee",
+    "url": "/SalemWebBeta/static/js/2.4ce7bfa2.chunk.js"
+  },
+  {
+    "revision": "894f2192d97a722b05c8",
+    "url": "/SalemWebBeta/static/js/main.bb9460bb.chunk.js"
+  },
+  {
+    "revision": "e5b712411421b8683a99",
+    "url": "/SalemWebBeta/static/js/runtime~main.5d5eeda8.js"
+  },
+  {
+    "revision": "d85514d4efbe5c9a128901b2ccf8ec1a",
+    "url": "/SalemWebBeta/static/media/logo.d85514d4.jpg"
+  }
+]);
